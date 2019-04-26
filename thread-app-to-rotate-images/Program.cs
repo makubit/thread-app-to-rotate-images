2dbd36d98f12a009bba3f22827ab5d04e9bc1071
KUBIT Magdalena <MKT9@dsone.3ds.com>
2019-04-26 15:38:41 +0200

    Init project

---
+++ thread-app-to-rotate-images/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread_app_to_rotate_images
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
